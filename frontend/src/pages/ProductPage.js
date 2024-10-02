5cf0cf477cab06011626241e506f4492e17a3243
Damirbey <dydamirbey@gmail.com>
2024-10-01 23:20:10 -0400

    Implemented Product Review Feature

--- frontend/src/pages/ProductPage.js
+++ frontend/src/pages/ProductPage.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { useEffect, useReducer } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from 'axios';
@@ -9,10 +9,19 @@ import Spinner from "../components/Spinner";
 import MessageBox from "../components/MessageBox";
 import { getError } from "../utils";
 import { Store } from "../Store";
+import { toast } from "react-toastify";
 
 //REDUCER TO MANAGE STATE
 const reducer = (state,action)=>{
     switch(action.type){
+        case 'CREATE_REQUEST':
+            return {...state, loadingCreate:true};
+        case 'CREATE_SUCCESS':
+            return {...state, loadingCreate:false};
+        case 'CREATE_FAIL':
+            return {...state, loadingCreate:false};
+        case 'REFRESH_PRODUCT':
+            return {...state, product:action.payload};
         case 'FETCH_REQUEST':
             return {...state, loading:true};
         case 'FETCH_SUCCESS':
@@ -33,6 +42,10 @@ function ProductPage(){
     });
     const {state, dispatch :ctxDispatch} = useContext(Store);
     const {cartItems} = state.cart;
+    const {userInfo} = state;
+    const [rating, setRating] = useState(0);
+    const [comment, setComment] = useState('');
+
     const navigate = useNavigate();
 
     useEffect(()=>{
@@ -59,7 +72,41 @@ function ProductPage(){
         ctxDispatch({type:'ADD_TO_CART', payload:{...product,quantity:quantity}});
         navigate("/cart");
     }
+
+    const onSubmitHandler = async(e)=>{
+        e.preventDefault();
+        if(!comment || !rating){
+            toast.error("Please enter comment and rating ");
+            return;
+        }
+
+        try{
+            dispatch({type:'CREATE_REQUEST'});
+
+            const { data } = await axios.post(
+                `/api/getProducts/${product._id}/reviews`,
+                { rating, comment, name: userInfo.name },
+                {
+                  headers: { Authorization: `Bearer ${userInfo.token}` },
+                }
+              );
+              dispatch({
+                type: 'CREATE_SUCCESS',
+              });
+              toast.success('Review submitted successfully');
+              product.reviews.unshift(data.review);
+              product.numReviews = data.numReviews;
+              product.rating = data.rating;
+              dispatch({ type: 'REFRESH_PRODUCT', payload: product });
+
+        }catch(err){
+            dispatch({type:'CREATE_FAIL'});
+            toast.error(getError(err));
+        }
+    }
+
     return <div className="product-screen"> 
+        <Helmet><title>{product.slug}</title></Helmet>    
         {
             loading ? 
             <Spinner/>
@@ -94,7 +141,52 @@ function ProductPage(){
                 </React.Fragment>
             )
         }
-        <Helmet><title>{product.slug}</title></Helmet>    
+
+        <div className="product-screen_reviews">
+            {product.reviews && product.reviews.length > 0 ?
+                <div>
+                    <h1>Reviews</h1>
+                    {product.reviews.map((review)=>
+                        <div className="reviewBox">
+                            <p className="reviewBox_name">{review.name}</p>
+                            <ProductRating rating={review.rating}/>
+                            <p className="reviewBox_comment">{review.comment}</p>
+
+                            <p className="reviewBox_date" style={{color:'#888'}}>
+                               <em>Posted on {new Date(review.createdAt).toLocaleDateString('en-GB', { day: 'numeric', month: 'long', year: 'numeric' })} </em>
+                            </p>
+                        </div>
+                    )}
+                    
+                </div>
+                
+                :
+                <MessageBox message={'No Reviews'} type="alert-info"/>
+                
+            }
+            {userInfo ? <div className="review-form">
+                    <h1>Write a customer review</h1>
+                    <form onSubmit={onSubmitHandler}>
+                        <select value={rating} 
+                            onChange={(e)=>setRating(e.target.value)}className="review-form_select">
+                            <option value="">Select...</option>
+                            <option value="1">1- Poor</option>
+                            <option value="2">2- Fair</option>
+                            <option value="3">3- Good</option>
+                            <option value="4">4- Very good</option>
+                            <option value="5">5- Excelent</option>
+                        </select>
+
+                        <textarea onChange={(e)=>setComment(e.target.value)}
+                            className="review-form_textarea" placeholder="Comments" rows="5"/>
+                        <button className="btn">Submit</button>
+                    </form>
+                    
+                </div>
+            :<MessageBox type="alert-warning" message={"Please to leave comment"} link={'/signIn'} linkMessage={'sign in'}></MessageBox>
+            }
+        </div>
+        
     </div>    
 } 
 
